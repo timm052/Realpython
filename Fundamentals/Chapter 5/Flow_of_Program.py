3a353b6dfe1017c73453912a61cc42670f164926
timm052 <timmooney052@gmail.com>
2018-01-17 02:21:59 +1100

    Program Flow added

---
+++ Fundamentals/Chapter 5/Flow_of_Program.py
@@ -0,0 +1,26 @@
+# Flow of Program
+
+# -------------------------------
+# Example 1
+# -------------------------------
+
+# so far there has been no way to allow decision making using boolens
+# combined with if staements to test if conditions are True or not
+# if statements will run if the conditions are evaluated to True
+if 2 + 2 == 4:
+    print("2 and 2 is 4")
+    print("Arithmetic works")
+
+# to set multiple if conditions and alternative case if none of the if or elif
+# statements are True then the else staement will run. When the code follows
+# one path it is known as branching.
+num = 15
+
+if num == 15:
+    print("Maths is broken")
+elif num <16:
+    print("nah it's cool maths seems to work")
+else:
+    print("yeah I have no idea what's going on")
+# NOTE: if there is an elif and if statement and both are true on the if
+# statement will execute. Also else and elif statements are optional
