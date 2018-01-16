fb5eabae0ad4a199745b7ddad4d35dfb73d2cc1b
timm052 <timmooney052@gmail.com>
2018-01-17 01:16:29 +1100

    Finished logic controls

--- Fundamentals/Chapter 5/add_logic.py
+++ Fundamentals/Chapter 5/add_logic.py
@@ -19,3 +19,31 @@ print(5 > 1 or 8 > 5)  # True
 print(5 > 1 or 3 > 4)  # True
 print(1 > 2 or 3 < 4)  # True
 print(1 > 2 or 3 > 4)  # False
+
+# not reverse the truth of a statement
+print(not True)
+print(not False)
+# more complex combinations are possible
+print(True and not (1 != 1))
+print(("A" != "A") or not (2 >= 3))
+
+
+# *******************************
+# Exercises
+# *******************************
+
+# Ex 1
+# (1 <= 1) and (1 != 1)
+# False
+
+# Ex 2
+# not (1 != 2)
+# False
+
+# Ex 3
+# ("good" != "bad") or False
+# False
+
+# Ex 4
+# ("good" != "Good") and not (1 == 1)
+# False
