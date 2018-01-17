93bfc1f54de5aba38a931c3b5fde65a06aa4c58f
timm052 <timmooney052@gmail.com>
2018-01-18 00:43:39 +1100

    formatting

--- Fundamentals/Chapter 5/Find_Factors_of_a_Number.py
+++ Fundamentals/Chapter 5/Find_Factors_of_a_Number.py
@@ -4,10 +4,11 @@ number = 0
 print("This program will find all the factors of a number")
 number = int(input("Enter a Number: "))
 
-for i in range(1, int(number/2) + 1):
+for i in range(1, int(number / 2) + 1):
     result = number % i
     if result == 0:
-        times = int(number/i)
+        #
+        times = int(number / i)
         print(f"{i} is a divisor of {number} and divides {times} times")
 
 print(f"{number} is a divisor of {number} and divides 1 time")
