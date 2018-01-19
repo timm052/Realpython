3199cb64ece491c0aabc1a70cb68255fbad02734
timm052 <timmooney052@gmail.com>
2018-01-19 23:39:49 +1100

    2/3 Done

--- Fundamentals/Chapter 6/lists_dictionary.py
+++ Fundamentals/Chapter 6/lists_dictionary.py
@@ -1,3 +1,44 @@
 # lists_dictioarys
 
-# lists are an object type
+# -------------------------------
+# Example 1
+# -------------------------------
+
+# lists are an object type that is able to hold other objects inside of them
+colours = ["red", "green", "burnt sienna", "blue"]
+scores = [10, 8, 9, -2, 9]
+my_list = ["one", 2, 3.0]  # lists can hold more than one type of object
+
+# getting a value out of a list is similar to getting a character form a string
+print(colours[2])
+print(colours[1:3])
+print(colours)
+
+#lists are multible which means we can change objects in the list
+colours[3] = "value changed"
+print(colours)
+
+# we can also also add/remove objects to aaready existing lists and create empty
+animals = []
+animals.append("lion")      # adding objects
+animals.append("tiger")
+animals.append("happy")
+animals.remove("happy")     # removing objects
+animals.extend(["cow", "dog" , "bird"]) # add mutiple items to a list
+print(animals)
+
+# the index() method can be used to find the index of a value in a list
+print(colours.index("green"))
+
+
+# -------------------------------
+# Example 2
+# -------------------------------
+
+# copying the contents of one list to anouther is more complicated simply
+# refrencing anouther list will not create a new list but rather a permanet
+# refrence to the first list
+large_cats = animals
+print(animals)
+animals.append("this should not be here")
+print(large_cats)
