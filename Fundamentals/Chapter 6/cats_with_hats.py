7c308c9e43d0acc6501ae6f74e01612b37d29c42
Tim Mooney <timmooney052@gmail.com>
2018-01-28 23:56:07 +1100

    first commit

---
+++ Fundamentals/Chapter 6/cats_with_hats.py
@@ -0,0 +1,25 @@
+# Cats with hats
+theCats = {}
+loops = 0
+
+#create 100 hatless cats in theCats dictionary
+for i in range(1, 101):
+    theCats["Cat " + str(i)] = False
+
+for i in range(1, 101):
+    loops += 1
+    count = 0
+    for cat, hats in theCats.items():
+        count += 1
+        if count % loops == 0:
+            if theCats[cat]:
+                theCats[cat] = False
+            else:
+                theCats[cat] = True
+
+# print results
+for cat, hats in theCats.items():
+    if theCats[cat]:
+        print(f"{cat} has a hat!")
+    else:
+        print(f"{cat} is hatless!")
