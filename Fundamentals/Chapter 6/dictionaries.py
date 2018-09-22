f7eaeb76ceaef5ba2d29995953b52214689bcb32
timm052 <timmooney052@gmail.com>
2018-09-23 02:34:11 +1000

    spellchecking
    Signed-off-by: timm052 <timmooney052@gmail.com>

--- Fundamentals/Chapter 6/dictionaries.py
+++ Fundamentals/Chapter 6/dictionaries.py
@@ -1,10 +1,10 @@
-# dictionarys
+# dictionaries
 
 # -------------------------------
 # Example 1
 # -------------------------------
 
-# dictionarys are used to store colloections of object like string or tuples
+# dictionaries are used to store colloections of object like string or tuples
 # however it stores the infomation in pairs called key-value pairs
 phonebook = {"Jenny": "867-5309", "Mike Jones": "281-330-8004",
              "Destiny": "900-783-3369"}
@@ -15,11 +15,11 @@ print(phonebook)
 # called hashing), but this ordering changes randomly every time the
 # contents of the dictionary change.
 
-# the important feasture of dictionarys is the paired value of each item in
+# the important feature of dictionaries is the paired value of each item in
 # in the list by specifying a key
 print(phonebook["Jenny"])
 
-# new keys can be added to a dictionary by spefying and new key and assigning a
+# new keys can be added to a dictionary by specifying and new key and assigning a
 # value
 phonebook["Obama"] = "202-456-1414"
 print(phonebook)
@@ -41,7 +41,7 @@ print(phonebook.keys())
 # -------------------------------
 
 # To preform an operation on all the keys in a dictionary we can use a for
-# loop the dictionary will be acessed in it's hashed order
+# loop the dictionary will be accessed in it's hashed order
 for contact_name in phonebook:
     print(contact_name, phonebook[contact_name])
 
@@ -49,14 +49,14 @@ for contact_name in phonebook:
 print("Jenny" in phonebook)
 print("Obama" in phonebook)
 
-# NOTE it's important to use in statement as requexting a dictionary key
+# NOTE it's important to use in statement as requesting a dictionary key
 # that does not exist will throw an error.
 
 # to acess the dictioary in its sorted order the sorted function is used
 for contact_name in sorted(phonebook):
     print(contact_name, phonebook[contact_name])
 
-# dictionary values can be any object type (including other dictionarys)
+# dictionary values can be any object type (including other dictionaries)
 # but keys must be immutable objects
 contacts = {"Jenny": {"cell": "555-0199", "home": "867-5309"},
             "Mike Jones": {"home": "281-330-8004"},
@@ -64,8 +64,8 @@ contacts = {"Jenny": {"cell": "555-0199", "home": "867-5309"},
 print(contacts)
 print(contacts["Jenny"]["cell"])
 
-# alternative ways to create dictionarys
-# dict() can be used to create dictionarys where key names will only contain
+# alternative ways to create dictionaries
+# dict() can be used to create dictionaries where key names will only contain
 # strings and numbers
 test_dictionary = dict(string1="value1", string2=2, string3=3.0)
 print(test_dictionary)
